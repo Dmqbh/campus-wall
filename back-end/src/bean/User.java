3d0927005fe8f9891e31d8a27c8f28950ea11cf6
huang <1459562870@qq.com>
2020-12-30 19:28:12 +0800

    用户的数据补全。

--- back-end/src/bean/User.java
+++ back-end/src/bean/User.java
@@ -1,5 +1,91 @@
 package bean;
 
+import java.util.Date;
+
 public class User {
-    String wfsdf;
+    /**
+     * 账号
+     * 密码
+     * 头像
+     * 邮箱
+     * 昵称
+     * 性别
+     * 注册时间
+     * 出生日期
+     */
+    int userid;
+    String password;
+    String avatar;
+    String mailbox;
+    String name;
+    String gender;
+    Date registration;
+    String birthday;
+
+    public User() {
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getMailbox() {
+        return mailbox;
+    }
+
+    public void setMailbox(String mailbox) {
+        this.mailbox = mailbox;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(Date registration) {
+        this.registration = registration;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
 }
