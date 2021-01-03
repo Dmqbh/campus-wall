2e78875bb211894e8f8e4c91ab63eb6a9cad7913
huang <1459562870@qq.com>
2021-01-03 20:01:46 +0800

    改post file

--- back-end/src/bean/Post.java
+++ back-end/src/bean/Post.java
@@ -1,4 +1,61 @@
 package bean;
 
+import java.util.Date;
+
 public class Post {
+    /**
+     * 文章号
+     * 标题
+     * 内容
+     * 发表用户账号
+     * 发表时间
+     */
+    int id;
+    String title;
+    String content;
+    int userid;
+    Date uptime;
+
+    public Post() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public Date getUptime() {
+        return uptime;
+    }
+
+    public void setUptime(Date uptime) {
+        this.uptime = uptime;
+    }
 }
