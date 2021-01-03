11b099b5e8448ab47432e90f1067d32dd60e3e94
huang <1459562870@qq.com>
2021-01-03 20:22:42 +0800

    new usersignin file

---
+++ back-end/src/web/UserSignIn.java
@@ -0,0 +1,11 @@
+package web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/user/login/signIn")
+public class UserSignIn extends HttpServlet {
+    /**
+     * 进行登入
+     */
+}
