d505cc7816eb3f9842ebb26e15fa8f5af966ed15
huang <1459562870@qq.com>
2021-01-03 20:40:18 +0800

    发表和显示博客文件的建立

---
+++ back-end/src/web/post/PostAdd.java
@@ -0,0 +1,11 @@
+package web.post;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/blog/add")
+public class PostAdd extends HttpServlet {
+    /**
+     * 发表博客
+     */
+}
---
+++ back-end/src/web/post/PostBlog.java
@@ -0,0 +1,10 @@
+package web.post;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/blog")
+public class PostBlog {
+    /**
+     * 显示博客主页
+     */
+}
