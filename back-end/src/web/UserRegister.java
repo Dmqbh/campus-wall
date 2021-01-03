bd2d9f866f7367b2469f2ae10a58a81313ea6fdd
huang <1459562870@qq.com>
2021-01-03 20:25:23 +0800

    new userregister file

---
+++ back-end/src/web/UserRegister.java
@@ -0,0 +1,11 @@
+package web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("user/register")
+public class UserRegister extends HttpServlet {
+    /**
+     * 注册
+     */
+}
